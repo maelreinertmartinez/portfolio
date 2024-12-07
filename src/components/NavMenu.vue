1128dec2a5f6d40b7256131adb363894b2795715
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2024-12-07 17:40:18 +0100

    Refactor NavMenu component: replace RouterLink with NavMenuItem for better encapsulation, and remove unused CSS styles related to link hover effects.

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import NavMenuItem from './NavMenuItem.vue'
 </script>
 
 <template>
   <nav
     class="flex flex-col gap-4 text-gray-600 border-y-8 border-l-8 p-12 rounded-l-xl w-max h-max border-orange-500"
   >
-    <RouterLink class="link w-max" to="/">About me</RouterLink>
-    <RouterLink class="link w-max" to="/skills">Skills</RouterLink>
-    <RouterLink class="link w-max" to="/projects">Projects</RouterLink>
+    <NavMenuItem class="w-max" to="/">About me</NavMenuItem>
+    <NavMenuItem class="w-max" to="/skills">Skills</NavMenuItem>
+    <NavMenuItem class="w-max" to="/projects">Projects</NavMenuItem>
   </nav>
 </template>
 
@@ -17,25 +17,5 @@ nav {
   box-shadow:
     inset 0 0 8px 0 rgba(0, 0, 0, 0.2),
     0 0 8px 0 rgba(0, 0, 0, 0.2);
-}
-
-.link {
-  position: relative;
-}
-
-.link::after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 0;
-  height: 1px;
-  background-color: #4b5563;
-  transition: width 0.3s ease-in-out;
-}
-
-.router-link-active::after,
-.link:hover::after {
-  width: 100%;
 }
 </style>
---
+++ src/components/NavMenuItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <RouterLink
+    class="link relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-px after:bg-gray-600 after:transition-all after:duration-300 after:ease-in-out"
+    :to="to"
+  >
+    <slot />
+  </RouterLink>
+</template>
+
+<style scoped>
+.router-link-active::after,
+.link:hover::after {
+  width: 100%;
+}
+</style>
