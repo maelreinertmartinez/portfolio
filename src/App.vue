d09b28f5aec637aeb34b9fc42eb342f44dce5d63
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2024-12-19 18:13:36 +0100

    adding of AppLauncher

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 import NavMenu from './components/NavMenu.vue'
 import CurrentCursor from './components/CurrentCursor.vue'
+import AppLauncher from './components/AppLauncher.vue'
+
+const isLaunched = ref(false)
+
+const launchApp = () => {
+  isLaunched.value = true
+}
 </script>
 
 <template>
-  <div class="main-app flex bg-timberwolf-200 w-full h-screen fixed overflow-hidden text-gray-700">
+  <div class="main-app bg-timberwolf-200 w-full h-screen fixed overflow-hidden text-gray-700">
     <CurrentCursor />
-    <NavMenu class="absolute top-8 -right-4" />
-    <RouterView />
+    <AppLauncher v-if="!isLaunched" @launch="launchApp" />
+    <div class="main-app w-full h-full" v-else>
+      <NavMenu class="absolute top-8 -right-4" />
+      <RouterView />
+    </div>
   </div>
 </template>
---
+++ src/components/AppLauncher.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center items-center h-full">
+    <button class="text-3xl font-bold tracking-wider p-4 rounded-lg" @click="$emit('launch')">
+      LAUNCH
+    </button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  border: 8px solid #f97316;
+}
+</style>
