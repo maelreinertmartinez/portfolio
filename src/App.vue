5314b6c2c925f6b4055f83a1267569a8db2585e5
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2024-12-22 23:05:13 +0100

    Reimplements app launcher logic
    Reactivates and refines app launcher component with conditional
    rendering based on launch state. Restores initialization logic
    for launching the app and updates template to use a dynamic
    structure for main content display based on launch status.
    
    Improves overall usability and prepares for further enhancements.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-// import { ref } from 'vue'
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 import NavMenu from './components/NavMenu.vue'
 import CurrentCursor from './components/CurrentCursor.vue'
-// import AppLauncher from './components/AppLauncher.vue'
+import AppLauncher from './components/AppLauncher.vue'
 import { SpeedInsights } from '@vercel/speed-insights/vue'
 import { Analytics } from '@vercel/analytics/nuxt'
 import { Direction } from './types/models'
 
-// const isLaunched = ref(false)
+const isLaunched = ref(false)
 
-// const launchApp = () => {
-//   isLaunched.value = true
-// }
+const launchApp = () => {
+  isLaunched.value = true
+}
 </script>
 
 <template>
   <div class="main-app bg-timberwolf-200 w-full h-screen fixed overflow-hidden text-gray-700">
     <CurrentCursor />
-    <!-- <AppLauncher v-if="!isLaunched" @launch="launchApp" /> -->
-    <div class="main-app w-full h-full">
+    <AppLauncher v-if="!isLaunched" @launch="launchApp" />
+    <div class="main-app w-full h-full" v-else>
       <NavMenu :position="Direction.Right" class="fixed top-8 -right-2" />
       <RouterView />
     </div>
